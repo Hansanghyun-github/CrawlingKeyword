0f449dfd9a5f1e16a48a1ac924342c8405633f69
HSH <han41562@gmail.com>
2024-06-25 18:50:19 +0900

    initial commit

---
+++ src/main/java/com/example/CrawlingKeyword/CrawlingKeywordApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrawlingKeyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingKeywordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlingKeywordApplication.class, args);
+	}
+
+}
