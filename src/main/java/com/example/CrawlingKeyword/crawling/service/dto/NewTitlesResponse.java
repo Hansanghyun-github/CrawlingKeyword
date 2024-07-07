561a0d2751b606c8b3a756694677a825c7049ba0
HSH <han41562@gmail.com>
2024-07-07 10:19:17 +0900

    fix: add constructor, getter annotation to response json object"

--- src/main/java/com/example/CrawlingKeyword/crawling/service/dto/NewTitlesResponse.java
+++ src/main/java/com/example/CrawlingKeyword/crawling/service/dto/NewTitlesResponse.java
@@ -2,10 +2,16 @@ package com.example.CrawlingKeyword.crawling.service.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class NewTitlesResponse {
     private List<String> inflearnNewTitles;
     private List<String> okkyNewTitles;
